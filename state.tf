d5bc6ba95519499bfa5dad50835e912391c97b73
Ubuntu <ubuntu@ip-172-31-94-74.ec2.internal>
2020-03-28 18:54:28 +0000

    ajuste bucket

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
+    bucket = "hackaton-fiap-1dvp-rm334479"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
